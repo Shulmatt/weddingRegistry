f2d6a975073fec5a8f17bfaa76afd3008bd7300c
shulmatt <matthews@MatthewsMacBook.localdomain>
2018-07-27 18:30:05 -0400

    Add registry

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RsvpComponent } from './rsvp/rsvp.component';
 import { GridComponent } from './grid/grid.component';
+import { RegistryComponent } from './registry/registry.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'rsvp', component: RsvpComponent },
-    { path: 'grid', component: GridComponent }
+    { path: 'grid', component: GridComponent },
+    { path: 'registry', component: RegistryComponent }
 ]
 
 @NgModule({
